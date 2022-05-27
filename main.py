50c4d83ad1efeca2f94acce056f4a77b7bcd3201
Yerenzter <73117946+Yerenzter@users.noreply.github.com>
2022-05-28 01:02:47 +0800

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 ## Create a function for a leap year.
 
-prompt = input('Enter a year: ')
+prompt = int(input('Enter a year: '))
 
 def leapYear(param_year):
-    if param_year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    if param_year % 4 == 0 and param_year % 100 != 0 or param_year % 400 == 0:
         print(str(param_year) + ' is a leap year.')
     else:
         print(str(param_year) + ' is  not a leap year.')
 
-prompt
+leapYear(prompt)
